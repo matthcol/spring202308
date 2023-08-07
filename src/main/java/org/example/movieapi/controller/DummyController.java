5b9c98c0b56596e2f4537c08cb22f3e60e650ce2
matthcol <matthias.colin@gmail.com>
2023-08-07 15:02:16 +0200

    query params

--- src/main/java/org/example/movieapi/controller/DummyController.java
+++ src/main/java/org/example/movieapi/controller/DummyController.java
@@ -1,23 +1,53 @@
 package org.example.movieapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/api/dummy")
 public class DummyController {
 
-    @GetMapping
-    String coucou(){
+    /**
+     * route: GET /api/dummy
+     * @return welcome message
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String coucou(){
         return "Coucou, je découvre Spring ??????????";
     }
 
+    /**
+     * route: GET /api/dummy/hour
+     * @return current date and time
+     */
     @GetMapping("hour")
-    LocalDateTime hour(){
+    public LocalDateTime hour(){
         return LocalDateTime.now();
+    }
+
+    /**
+     * route: POST /api/dummy
+     * @param message provided with query param 'm'
+     * @return same message with prefix 'say:'
+     */
+    @PostMapping
+    public String mirrorQueryParam(
+            @RequestParam("m") String message,
+            @RequestParam(name = "n", required = false, defaultValue = "1") int count,
+            @RequestParam(name = "d", required = false) LocalDate date
+    ){
+        var messageRepeated = IntStream.range(0, count)
+                .mapToObj(i -> message)
+                .collect(Collectors.joining(", "));
+        return MessageFormat.format("say: {0} ({1})",
+                messageRepeated,
+                date
+        );
     }
 }
