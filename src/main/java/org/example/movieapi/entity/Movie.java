de6423915574783a7a90f8cecc9770f2b1df6d89
matthcol <matthias.colin@gmail.com>
2023-08-09 10:11:44 +0200

    entity Movie

--- src/main/java/org/example/movieapi/entity/Movie.java
+++ src/main/java/org/example/movieapi/entity/Movie.java
@@ -1,4 +1,22 @@
 package org.example.movieapi.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+        name = "movies",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"title", "year"})
+)
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 300)
+    private String title;
+    private short year;
+    private Short duration;
+
+    @Column(length = 4000)
+    private String synopsis;
 }
