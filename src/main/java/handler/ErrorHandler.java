6526c8b1b737718917e09c6998f9722cada6a289
matthcol <matthias.colin@gmail.com>
2023-08-10 12:19:24 +0200

    postman collection updated

--- src/main/java/handler/ErrorHandler.java
+++ src/main/java/handler/ErrorHandler.java
@@ -2,6 +2,7 @@ package handler;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+/*
+ * Articles:
+ * https://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc
+ * https://www.baeldung.com/exception-handling-for-rest-with-spring
+ */
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
@@ -22,9 +28,11 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 //        return super.handleMethodArgumentNotValid(ex, headers, status, request);
 //    }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<String> handleDaoDataIntegrityViolationException(Exception exception, WebRequest request){
-        return ResponseEntity.status(HttpStatus.CONFLICT)
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> handleDaoDataIntegrityViolationException(
+            Exception exception, WebRequest request)
+    {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body("Data Integrity Violation");
     }
 }
