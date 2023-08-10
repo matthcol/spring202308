82b5adec35016fa6499360ed06ea5bc145935084
matthcol <matthias.colin@gmail.com>
2023-08-10 10:37:44 +0200

    implements all methods  MovieServiceJpa

--- src/main/java/org/example/movieapi/service/jpa/MovieServiceJpa.java
+++ src/main/java/org/example/movieapi/service/jpa/MovieServiceJpa.java
@@ -1,16 +1,22 @@
 package org.example.movieapi.service.jpa;
 
+import jakarta.persistence.criteria.Order;
 import org.example.movieapi.dto.MovieCreate;
 import org.example.movieapi.dto.MovieDetail;
 import org.example.movieapi.dto.MovieSimple;
+import org.example.movieapi.entity.Movie;
 import org.example.movieapi.repository.MovieRepository;
 import org.example.movieapi.service.MovieService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.data.domain.Sort.Order.asc;
+import static org.springframework.data.domain.Sort.Order.desc;
 
 @Service
 public class MovieServiceJpa implements MovieService {
@@ -29,21 +35,37 @@ public class MovieServiceJpa implements MovieService {
 
     @Override
     public List<MovieSimple> getAll() {
-        return null;
+        return movieRepository.findAll(Sort.by(desc("year"), asc("title")))
+                .stream()
+                .map(movieEntity -> modelMapper.map(movieEntity, MovieSimple.class))
+                .toList();
     }
 
     @Override
     public MovieSimple add(MovieCreate movie) {
-        return null;
+        var movieEntity = modelMapper.map(movie, Movie.class);
+        movieRepository.saveAndFlush(movieEntity);
+        return modelMapper.map(movieEntity, MovieSimple.class);
     }
 
     @Override
     public Optional<MovieDetail> update(MovieCreate movie, int id) {
-        return Optional.empty();
+        return movieRepository.findById(id)
+                .map(movieEntity -> {
+                    modelMapper.map(movie, movieEntity);
+                    movieRepository.flush(); // update SQL
+                    return modelMapper.map(movieEntity, MovieDetail.class);
+                });
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        return movieRepository.findById(id)
+                .map(movieEntity -> {
+                    movieRepository.delete(movieEntity);
+                    movieRepository.flush();
+                    return true;
+                })
+                .orElse(false);
     }
 }
